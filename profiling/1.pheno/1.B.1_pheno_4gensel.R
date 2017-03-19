bc04aa77ff5dd4fe759eff9a3e8eebb0dbbafdeb
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-03-19 12:49:25 -0700

    pheno trait for 5x cv

--- profiling/1.pheno/1.B.1_pheno_4gensel.R
+++ profiling/1.pheno/1.B.1_pheno_4gensel.R
@@ -1,16 +1,17 @@
 ## Jinliang Yang
-## Oct. 13th, 2014
+## 03-19-2017
 ## phenotypic data of offpvp diallel
 
 #setwd("~/Documents/Github/pvpDiallel/")
-trait2gs <- function(outdir="largedata/pheno/wholeset/"){
+trait2gs <- function(traitfile = "data/hyb_heterosis.csv", outdir="largedata/pheno/wholeset/"){
   
-  trait <- read.csv("data/trait_matrix_updated_BPH.csv")
-  trait$Hyb <- paste(trait$P1, trait$P2, sep="x")
+  trait <- read.csv(traitfile)
+  trait$Hyb <- paste(trait$Par1, trait$Par2, sep="x")
+  dir.create(outdir, showWarnings = FALSE)
   
   ti <- c("ASI", "DTP", "DTS", "EHT",  "GY", "PHT",  "TW")
-  ts <- c("valHyb", "BPHmax", "pBPHmax", "BPHmin", "pBPHmin", "MPH", "pMPH", "pBPH", "BPH")
-  
+  #ts <- c("valHyb", "BPHmax", "pBPHmax", "BPHmin", "pBPHmin", "MPH", "pMPH", "pBPH", "BPH")
+  ts <- c("valHyb", "pMPH", "MPH")
   for(i in 1:7){
     pheno <- subset(trait, trait==ti[i])
     
@@ -19,17 +20,17 @@ trait2gs <- function(outdir="largedata/pheno/wholeset/"){
     names(pheno1) <- c("Genotype", ti[i], "Fix")
     pheno1$Fix <- 1
     
-    out <- paste(outdir, tolower(ti[i]), "_perse.txt", sep="")
+    out <- paste(outdir, "/", tolower(ti[i]), "_perse.txt", sep="")
     write.table(pheno1, out, sep="\t", row.names=FALSE, quote=FALSE) 
     message(sprintf(">>> output [ %s ] per se pheno to [ %s ]", ti[i], out)) 
     
     ### ==> other traits
-    for(j in 2:9){
-      pheno1 <- pheno[, c("Hyb", ts[j], "valP1")]
+    for(j in 2:3){
+      pheno1 <- pheno[, c("Hyb", ts[j], "valPar1")]
       names(pheno1) <- c("Genotype", ti[i], "Fix")
       pheno1$Fix <- 1
       
-      out <- paste(outdir, tolower(ti[i]), "_", ts[j], ".txt", sep="")
+      out <- paste(outdir, "/", tolower(ti[i]), "_", ts[j], ".txt", sep="")
       write.table(pheno1, out, sep="\t", row.names=FALSE, quote=FALSE) 
       message(sprintf(">>> output [ %s ] | [ %s ] pheno to [ %s ]", ti[i], ts[j], out)) 
       
@@ -37,4 +38,4 @@ trait2gs <- function(outdir="largedata/pheno/wholeset/"){
   } 
 }
 
-trait2gs(outdir="largedata/pheno/wholeset/")
+trait2gs(traitfile = "data/hyb_heterosis.csv", outdir="largedata/pheno/wholeset/")
