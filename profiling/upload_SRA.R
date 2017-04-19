c6559bdd999209d788db21e5ec5a399e72867f7b
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-04-19 16:56:44 -0700

    updated doc/ for Fig1 and Fig2

--- profiling/upload_SRA.R
+++ profiling/upload_SRA.R
@@ -1,7 +1,7 @@
 
 library("farmeR")
 
-cmd <- paste0("ascp -i ~/bin/aspera.openssh -Tr -l100m -k1 ",
+cmd <- paste0("ascp -i ~/bin/aspera.openssh -TQ -l100m -k1 ",
               "-d /group/jrigrp4/diallel_fq/fastq ",
               "subasp@upload.ncbi.nlm.nih.gov:uploads/yangjl0930@gmail.com_7MfdNojq/GERP-diallel/")
 
@@ -13,3 +13,6 @@ set_farm_job(slurmsh = "slurm-script/upload_sra.sh",
 ###>>> RUN: sbatch -p bigmemh --mem 5G --ntasks=1 --time=80:00:00 slurm-script/upload_sra.sh
 
 ## ssh farm -p 2022
+
+# rm 2920_3807_2920_Y_RIMMA0806-1_R2.fastq.gz
+# rm 2918_3807_2918_N_RIMMA0804.1_R2.fastq.gz
